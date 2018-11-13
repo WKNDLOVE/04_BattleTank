ef849342b37ecc39f1e026712b7a49b88398a312
DESKTOP-SAOJTFR\Liberty <dominik.palmitessa@gmx.ch>
2018-11-13 17:45:10 +0100

    13.11.2018 AIController setup that detects playercontroller / Starting with getting an out parameter for aimcursor

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,6 @@
 #include "TankPlayerController.h"
 
 
-
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -15,13 +14,40 @@ void ATankPlayerController::BeginPlay()
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("PLayerController is possesing: %s"),*(ControlledTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("PLayerController is possesing: %s"), *(ControlledTank->GetName()));
 
 	}
-	
+
 }
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+	UE_LOG(LogTemp, Warning, TEXT("Player controller ticking"))
+}
+
 
 ATank * ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+	
+	FVector OutHitLocation; // Out parameter
+	if (GetSightRayHitLocation(OutHitLocation)) // has "side-effect", is going to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Outhitlocation: %s"), *OutHitLocation.ToString());
+	}
+		// TODO tell controlled tank to aim at this point
+}
+
+//Get world location of linetrace through corsshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	OutHitLocation = FVector(1.0);
+	return true;
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -19,4 +19,13 @@ public:
 	ATank * GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	// Start the tank moving the barrel so that a shot would hit where the crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 };
